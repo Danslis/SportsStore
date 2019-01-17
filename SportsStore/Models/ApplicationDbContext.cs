cfdfdc0cccb5f5f2fc02880f2c8b499d34e0a130
Dart <Danslis@yandex.ru>
2019-01-17 14:06:58 +0300

    step1

--- SportsStore/Models/ApplicationDbContext.cs
+++ SportsStore/Models/ApplicationDbContext.cs
@@ -1,11 +1,68 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace SportsStore.Models
 {
-    public class ApplicationDbContext
+
+    public class ApplicationDbContext : DbContext
     {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+
+    public class ApplicationDbContextFactory
+            : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+
+        public ApplicationDbContext CreateDbContext(string[] args) =>
+            Program.BuildWebHost(args).Services
+                .GetRequiredService<ApplicationDbContext>();
     }
 }
+
+
+
+
+
+
+
+/*using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace SportsStore.Models
+{
+
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            builder.UseSqlServer("Server=(local)\\SQLEXPRESS;Database=yourdatabase;User ID=RD;Password=password;TrustServerCertificate=True;Trusted_Connection=False;Connection Timeout=30;Integrated Security=False;Persist Security Info=False;Encrypt=True;MultipleActiveResultSets=True;",
+                optionsBuilder => optionsBuilder.MigrationsAssembly(typeof(ApplicationDbContext).GetTypeInfo().Assembly.GetName().Name));
+
+            return new ApplicationDbContext(builder.Options);
+        }
+    }
+    /*public class ApplicationDbContextFactory
+            : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+
+        public ApplicationDbContext CreateDbContext(string[] args) =>
+            Program.BuildWebHost(args).Services
+                .GetRequiredService<ApplicationDbContext>();
+    }*/
+//}*/
--- SportsStore/Models/EFProductRepository.cs
+++ SportsStore/Models/EFProductRepository.cs
@@ -5,7 +5,13 @@ using System.Threading.Tasks;
 
 namespace SportsStore.Models
 {
-    public class EFProductRepository
+    public class EFProductRepository : IProductRepository
     {
+        private ApplicationDbContext context;
+        public EFProductRepository(ApplicationDbContext ctx)
+        {
+            context = ctx;
+        }
+        public IQueryable<Product> Products => context.Products;
     }
 }
